3ebcf64995f08555f1f3a095dea0b9e046459abb
Alir3za <alireza2212kazemi@gmail.com>
2024-11-11 22:00:05 +0330

    simple user and pass checkup

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+// a simple modal mode for checking user and pass
+
+// let visitor = prompt("who are you?");
+// let pass = "" ;
+
+// if (visitor == "Admin") {
+//   pass = prompt("whats the pass?", "");
+//   (pass == "TheMaster") ? alert("Welcome") :
+//   (pass == "" || null) ? alert("Canceled") :
+//   alert("Wrong pass!");
+// } else if (visitor == null){
+//   alert("Canceled");
+// } else {
+//   alert("I dont know you");
+// }
+
+/////////////////////////////////////////////////
+
+
