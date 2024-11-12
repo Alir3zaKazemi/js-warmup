721f58d066dee3f1cf06871f4da1e85d0b806847
Alir3za <alireza2212kazemi@gmail.com>
2024-11-12 15:28:11 +0330

    x to the power of y (function article full filled)

--- scripts.js
+++ scripts.js
@@ -48,12 +48,57 @@
 
 // let n = +prompt("enter value for n: ");
 
-// out: for (let i = 1; i < n; i++) {
+// out: for (let i = 1; i <= n; i++) {
 //   for (let j = 2; j < i; j++) {
 //     if(i % j == 0) continue out;
 //   }
-//   alert(i);
+//   console.log(i);
 // }
 
 //////////////////////////////////////////////////
+
+// not using break for two same conditions
+
+// let a = +prompt("a?");
+
+// switch (a) {
+//   case 0:
+//     console.log(0);
+//     break;
+//   case 1:
+//     console.log(1);
+//     break;
+//   case 2:
+//   case 3:
+//     console.log("2,3")
+//     break;
+//   default:
+//     console.log("its large")
+//     break;
+// }
+
+//////////////////////////////////////////////////////
+
+// x to the power of y
+
+// function pow(x,y) {
+//   let result = 1;
+//   for (let i = 1; i <= y; i++) {
+//     result *= x;
+//   }
+//   return result;
+// }
+
+// let a = +prompt("enter num 1:");
+// let b = +prompt("enter num 2:");
+
+// b < 1
+// 	? console.log(
+// 			pow(a, +prompt("num less than 1 is invalid. enter the b once more"))
+// 	  )
+// 	: console.log(pow(a, b));
+
+// alert("check the value in console ");
+
+/////////////////////////////////////////////////////
 
